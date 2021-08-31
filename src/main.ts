c7936a5bbb354c6bae313cfbd56792e399f7f280
kris <wupengfeibj@yuanfudao.com>
2021-08-31 11:27:52 +0800

    feat: use ejs view

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  app.setViewEngine('ejs');
 
   await app.listen(3000);
 }
