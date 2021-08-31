005a59fca462866dbb6576b81d0f138aab20ad83
kris <wupengfeibj@yuanfudao.com>
2021-08-31 11:06:34 +0800

    feat: support dynamic mvc

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private appService: AppService) {}
+
+  @Get()
+  root(@Res() res: Response) {
+    return res.render(this.appService.getViewName(), {
+      message: this.appService.getHello(),
+    });
+  }
+}
