73fd397fe7c13af20891cbd727fe0d5ca19458c8
kris <wupengfeibj@yuanfudao.com>
2021-09-01 16:34:15 +0800

    feat: use 8080

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
